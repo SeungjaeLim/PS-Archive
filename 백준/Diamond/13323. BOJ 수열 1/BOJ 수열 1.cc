c9b5e041fb790da0a806f19828f209b61969e0bd
Seungjae Lim <seungjaelim@kaist.ac.kr>
2023-10-13 19:10:50 +0900

    [Diamond III] Title: BOJ 수열 1, Time: 316 ms, Memory: 7384 KB -BaekjoonHub

---
+++ 백준/Diamond/13323. BOJ 수열 1/BOJ 수열 1.cc
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <queue>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int n;
+    int num;
+    long long result = 0;
+    priority_queue<int> pq;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &num);
+        num -= i;
+        pq.push(num);
+
+        if (!pq.empty() && pq.top() > num) {
+            result += pq.top() - num;
+            pq.pop();
+            pq.push(num);
+        }
+    }
+    printf("%lld\n", result);
+
+    return 0;
+}
