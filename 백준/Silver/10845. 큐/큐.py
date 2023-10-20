d1c80c5246d7c0778a1e2c86a8d53b7930b778bf
Seungjae Lim <seungjaelim@kaist.ac.kr>
2023-10-20 16:35:44 +0900

    [Silver IV] Title: 큐, Time: 72 ms, Memory: 34036 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,36 @@
+from collections import deque
+import sys
+
+n = int(input())
+
+queue = deque()
+
+for _ in range(n):
+    command = sys.stdin.readline().strip().split()
+
+    if command[0] == 'push':
+        queue.append(int(command[1]))
+
+    elif command[0] == 'pop':
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+
+    elif command[0] == 'size':
+        print(len(queue))
+
+    elif command[0] == 'empty':
+        print(1 if not queue else 0)
+
+    elif command[0] == 'front':
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+
+    elif command[0] == 'back':
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
